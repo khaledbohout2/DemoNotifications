08d3f754d98c82104a57687c3bb1f984b6bdfc51
Khaled Bohout <amrutaka@MacBook-Pro-alkhas-b-amr.local>
2019-02-13 20:26:45 +0200

    Initial Commit

---
+++ Notifications/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notifications
+//
+//  Created by Khaled Bohout on 2/13/19.
+//  Copyright © 2019 Khaled Bohout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
